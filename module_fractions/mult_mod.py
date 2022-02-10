64f805e401e957df866507275dd3b2df8aaa9ecb
«Buldakova <79263143446@mail.ru>
2022-02-10 22:00:43 +0300

    module_mult

---
+++ module_fractions/mult_mod.py
@@ -0,0 +1,16 @@
+from fractions import Fraction
+
+
+num_1 = Fraction() 
+num_2 = Fraction()
+
+
+def init(a,b):
+    global num_1
+    global num_2 
+    num_1 = a
+    num_2 = b
+    
+
+def do_it():
+    return num_1 * num_2
