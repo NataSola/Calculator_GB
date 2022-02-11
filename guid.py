2596a3a53bd1d3d61d6a529fb987541c9973cbad
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-02-11 23:58:07 +0300

    Add guid

---
+++ guid.py
@@ -0,0 +1,6 @@
+import module_fractions.div_mod as fdiv
+import module_fractions.mult_mod as fmul
+import module_fractions.subt_mod as fsub
+import module_fractions.sum_mod as fsum
+
+dict_fract = {'/': fdiv, '*': fmul, '-': fsub, '+': fsum}
