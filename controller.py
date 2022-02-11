2ba3b87c30aad74e775c8c78cf5d4542c83aaf45
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-02-12 00:17:27 +0300

    fixed bugs

--- controller.py
+++ controller.py
@@ -12,12 +12,12 @@ import guid
 
 def button_click():
     value_a = view.get_value()
-    oper = view.get_operator
+    oper = view.get_operator()
     value_b = view.get_value()
     func = guid.dict_fract[oper]
     func.init(value_a, value_b)
     result = func.do_it()
-    view.get_result(result)
+    view.get_result(f'Результат: {result}')
 
 
 
