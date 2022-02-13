fc99733580ced145086285e29662dea7cf5a7731
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-02-13 14:34:04 +0300

    Change for logging

--- controller.py
+++ controller.py
@@ -1,7 +1,3 @@
-import module_fractions.div_mod as fdiv
-import module_fractions.mult_mod as fmul
-import module_fractions.subt_mod as fsub
-import module_fractions.sum_mod as fsum
 import view
 import guid
 
--- guid.py
+++ guid.py
@@ -3,4 +3,9 @@ import module_fractions.mult_mod as fmul
 import module_fractions.subt_mod as fsub
 import module_fractions.sum_mod as fsum
 
+
 dict_fract = {'/': fdiv, '*': fmul, '-': fsub, '+': fsum}
+dict_log = {'/': 'Деление', '*': 'Умножение', '-': 'Вычитание', '+': 'Сложение'}
+
+
+
